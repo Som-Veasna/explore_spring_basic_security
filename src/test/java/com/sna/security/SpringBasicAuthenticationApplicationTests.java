c46a267bb7e5e380cdb7b51afc5c03d6c55aa1ee
Veasna <somveasna00@gmail.com>
2026-03-23 11:34:33 +0700

    add spring basic security

---
+++ src/test/java/com/sna/security/SpringBasicAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sna.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBasicAuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
